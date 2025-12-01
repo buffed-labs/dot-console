13da7917d5675e5f84bda596f356ad4232598bb9
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-12-02 02:32:53 +1300

    fix: incorrect `Await` component

--- src/routes/_layout/_no-custom/collectives/_layout.tsx
+++ src/routes/_layout/_no-custom/collectives/_layout.tsx
@@ -1,13 +1,15 @@
 import { idle } from "@reactive-dot/core";
-import { useLazyLoadQuery } from "@reactive-dot/react";
-import { Await, createFileRoute } from "@tanstack/react-router";
+import { Await, useLazyLoadQuery } from "@reactive-dot/react";
+import { createFileRoute } from "@tanstack/react-router";
 import { Suspense } from "react";
 import { RouteTabs } from "~/components/route-tabs";
 import { useCollectivesChainId } from "~/hooks/chain";
 
-export const Route = createFileRoute("/_layout/_no-custom/collectives/_layout")({
-  component: CollectivesPage,
-});
+export const Route = createFileRoute("/_layout/_no-custom/collectives/_layout")(
+  {
+    component: CollectivesPage,
+  },
+);
 
 function CollectivesPage() {
   const collectivesChainId = useCollectivesChainId(false);
